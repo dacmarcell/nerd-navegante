71c210307317df7261cabb6ce80c88591aa2138d
marcelldac <devmarcell@protonmail.com>
2024-07-05 19:10:38 -0300

    feat: implements authentication

---
+++ src/pages/verify-login.ts
@@ -0,0 +1,24 @@
+import { type APIContext, type APIRoute } from "astro";
+import jwt from "jsonwebtoken";
+
+export const POST: APIRoute = async ({ request, cookies }: APIContext) => {
+  const formData: FormData = await request.formData();
+  const username: string = formData.get("username") as string;
+  const password: string = formData.get("password") as string;
+
+  if (
+    username !== "devmarcell" ||
+    password !== "f12456145ae5b1f6489801e8bbfb99f8244c5546"
+  ) {
+    return new Response("Unauthorized!", { status: 401 });
+  }
+
+  const token: string = jwt.sign(
+    { username },
+    import.meta.env.JWT_SECRET as string
+  );
+
+  cookies.set("token", token, { httpOnly: true, path: "/" });
+
+  return new Response(token, { status: 200 });
+};
