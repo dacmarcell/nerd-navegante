b748e0dbb4a59107c52629e20039a5ff8b56afd1
marcelldac <devmarcell@protonmail.com>
2024-07-05 20:34:57 -0300

    feat: add env d ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+import type { App } from "astro/app";
+//TODO: Check if this is dead code
+declare global {
+  namespace App {
+    interface Locals {
+      username: string;
+    }
+  }
+}
