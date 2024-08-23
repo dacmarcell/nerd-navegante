385b5035216b5159b16c3cabbbf82b65a39f7760
Developer Marcell <devmarcell@protonmail.com>
2024-08-23 01:29:43 -0300

    chore: add Vercel adapter for serverless deployment with web analytics enabled

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +17,9 @@ export default defineConfig({
     react(),
     sitemap(),
   ],
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   markdown: {
     remarkPlugins: [
       remarkToc,
